0094c78405856d4e4e1d0c864db2c3728d33917c
Sam Plackett <samuel.plackett@eviden.com>
2023-11-02 16:17:28 +0000

    add control file generation

--- app/processing/publish.js
+++ app/processing/publish.js
@@ -2,6 +2,7 @@ const { getOutboundBlobClient } = require('../storage')
 const { writeToString } = require('@fast-csv/format')
 const { getNewFileName } = require('./get-new-filename')
 const { getNewContent } = require('./get-new-content')
+const { DAX } = require('../constants/file-types')
 
 const publish = async (content, filename, fileType) => {
   filename = getNewFileName(filename, fileType)
@@ -9,6 +10,11 @@ const publish = async (content, filename, fileType) => {
   const body = await writeToString(newContent)
   const outboundBlobClient = await getOutboundBlobClient(filename, fileType)
   await outboundBlobClient.upload(body, body.length)
+  if (fileType === DAX) {
+    const ctlFileName = 'CTL_' + filename
+    const ctlOutboundBlobClient = await getOutboundBlobClient(ctlFileName, fileType)
+    await ctlOutboundBlobClient.upload('', 0)
+  }
   console.info(`Published ${filename}`)
 }
 
