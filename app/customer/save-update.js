90d21a95ca64e66818229a1b7b6951cc5252df4f
Sam Plackett <samuel.plackett@eviden.com>
2024-01-12 15:22:47 +0000

    added tests, fixed minor issues

--- app/customer/save-update.js
+++ app/customer/save-update.js
@@ -6,7 +6,7 @@ const saveUpdate = async (customerUpdate) => {
     if ([TRADER].includes(referenceType)) {
       const existingCustomer = await db.customer.findOne({ where: { trader: customerUpdate[referenceType].toString() } })
       if (existingCustomer) {
-        await db.customer.update({ frn: customerUpdate.frn }, { where: { id: existingCustomer.id } })
+        await db.customer.update({ frn: customerUpdate.frn }, { where: { customerId: existingCustomer.customerId } })
       } else {
         await db.customer.create({ trader: customerUpdate[referenceType], frn: customerUpdate.frn })
       }
