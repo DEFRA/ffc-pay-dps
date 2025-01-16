a386687fde130cc0f42825848e345fa1ace35c7c
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2025-01-16 17:30:34 +0000

    will this exra sonar coverage reduce it again? who knows

--- test/unit/processing/dax/transform-line.test.js
+++ test/unit/processing/dax/transform-line.test.js
@@ -1,67 +1,91 @@
 const { DAX } = require('../../../../app/constants/file-types')
-const { transformLine } = require('../../../../app/processing/dax/transform-line')
+const {
+  transformLine,
+  BATCH_LINES
+} = require('../../../../app/processing/dax/transform-line')
 const filename = require('../../../mocks/filename')
 
-const batchLine = ['010540', '010540', '1RB13915', 'Yes', 'GT0362947', 'Transfer of goods', 'Yes']
+const batchLine = [
+  '010540',
+  '010540',
+  '1RB13915',
+  'Yes',
+  'GT0362947',
+  'Transfer of goods',
+  'Yes'
+]
 
 describe('Transform DAX line', () => {
-  test('returns correlationId', async () => {
-    const result = transformLine(batchLine, filename.DAX)
+  let result
+
+  beforeEach(() => {
+    result = transformLine(batchLine, filename.DAX)
+  })
+
+  test('returns correlationId', () => {
     expect(result.correlationId).not.toBe(null)
   })
 
-  test('returns batch', async () => {
-    const result = transformLine(batchLine, filename.DAX)
+  test('returns batch', () => {
     expect(result.batch).toBe(filename.DAX)
   })
 
-  test('returns fileTypeId of DAX ID', async () => {
-    const result = transformLine(batchLine, filename.DAX)
+  test('returns fileTypeId of DAX ID', () => {
     expect(result.fileTypeId).toBe(DAX.fileTypeId)
   })
 
-  test('returns primaryTrader', async () => {
-    const result = transformLine(batchLine, filename.DAX)
+  test('returns primaryTrader', () => {
     expect(result.primaryTrader).toBe('010540')
   })
 
-  test('returns usedByTrader', async () => {
-    const result = transformLine(batchLine, filename.DAX)
+  test('returns usedByTrader', () => {
     expect(result.usedByTrader).toBe('010540')
   })
 
-  test('returns reference', async () => {
-    const result = transformLine(batchLine, filename.DAX)
+  test('returns reference', () => {
     expect(result.reference).toBe('1RB13915')
   })
 
-  test('returns loaded', async () => {
-    const result = transformLine(batchLine, filename.DAX)
+  test('returns loaded status', () => {
     expect(result.loaded).toBe('Yes')
   })
 
-  test('returns daxReference', async () => {
-    const result = transformLine(batchLine, filename.DAX)
+  test('returns daxReference', () => {
     expect(result.daxReference).toBe('GT0362947')
   })
 
-  test('returns reason', async () => {
-    const result = transformLine(batchLine, filename.DAX)
+  test('returns reason', () => {
     expect(result.reason).toBe('Transfer of goods')
   })
 
-  test('returns posted', async () => {
-    const result = transformLine(batchLine, filename.DAX)
+  test('returns posted status', () => {
     expect(result.posted).toBe('Yes')
   })
 
-  test('returns correlationId even if batchLine is empty', async () => {
-    const result = transformLine([], filename.DAX)
-    expect(result.correlationId).not.toBe(null)
+  test('returns complete object structure', () => {
+    expect(result).toEqual({
+      correlationId: expect.any(String),
+      batch: filename.DAX,
+      fileTypeId: DAX.fileTypeId,
+      primaryTrader: '010540',
+      usedByTrader: '010540',
+      reference: '1RB13915',
+      loaded: 'Yes',
+      daxReference: 'GT0362947',
+      reason: 'Transfer of goods',
+      posted: 'Yes'
+    })
   })
 
-  test('returns batch even if batchLine is empty', async () => {
-    const result = transformLine([], filename.DAX)
-    expect(result.batch).toBe(filename.DAX)
+  test('BATCH_LINES constant is exported correctly', () => {
+    expect(BATCH_LINES).toEqual({
+      PRIMARY_TRADER: 0,
+      USED_BY_TRADER: 1,
+      REFERENCE: 2,
+      LOADED: 3,
+      DAX_REFERENCE: 4,
+      REASON: 5,
+      POSTED: 6
+    })
   })
 })
