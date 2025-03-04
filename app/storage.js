d11881052f5b646f958cb5957ae8942722ce8582
Oliver Lewington <oliver.lewington@eviden.com>
2025-03-04 11:57:23 +0000

    Fix storage.js issue

--- app/storage.js
+++ app/storage.js
@@ -13,7 +13,7 @@ if (storageConfig.useConnectionStr) {
 } else {
   console.log('Using DefaultAzureCredential for BlobServiceClient')
   const uri = `https://${storageConfig.storageAccount}.blob.core.windows.net`
-  blobServiceClient = new BlobServiceClient(uri, new DefaultAzureCredential({ managedIdentityClientId: config.managedIdentityClientId }))
+  blobServiceClient = new BlobServiceClient(uri, new DefaultAzureCredential({ managedIdentityClientId: storageConfig.managedIdentityClientId }))
 }
 
 const container = blobServiceClient.getContainerClient(storageConfig.dpsContainer)
